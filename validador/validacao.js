80b0d9b3d459c9054cb35762582439d55c31c59e
brunoserra <brunorrrossi@gmail.com>
2021-09-26 16:41:04 -0300

    validar sobrenome
    sobrenome

--- validador/validacao.js
+++ validador/validacao.js
@@ -1,12 +1,13 @@
 function enviar() {
 
     var nome = document.getElementById('nome');
-    if (nome.value == "" &&
+    if (nome.value == "" ||
         nome.value.length < 3) {
         alert('por favor digite seu nome');
         return;
 
     }
+    var sobrenome = document.getElementById('sobrenome')
 
 
 
