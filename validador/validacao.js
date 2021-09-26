bc29901fb73962efaa95e1e1b45f56f9ae75851e
brunoserra <brunorrrossi@gmail.com>
2021-09-26 15:51:43 -0300

    adiconado javascript
    javascript

---
+++ validador/validacao.js
@@ -0,0 +1,14 @@
+function enviar() {
+
+    var nome = document.getElementById('nome');
+    if (nome.value == "" &&
+        nome.value.length < 3) {
+        alert('por favor digite seu nome');
+        return;
+
+    }
+
+
+
+
+}
