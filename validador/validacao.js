85d68a685b56f3e3f4a871a76bdfe37273db103a
brunoserra <brunorrrossi@gmail.com>
2021-09-26 19:05:16 -0300

    validador
    cep

--- validador/validacao.js
+++ validador/validacao.js
@@ -2,7 +2,8 @@ function enviar() {
 
     var nome = document.getElementById('nome');
     var sobrenome = document.getElementById('sobrenome');
-    varrg = document.getElementsByTagName('rg');
+    var rg = document.getElementsByTagName('rg');
+    var cpf = document.createElement('cpf');
     //esta parte valida o nome
     if (nome.value == "" ||
         nome.value.length < 3) {
@@ -17,6 +18,7 @@ function enviar() {
         sobrenome.focus();
         return;
     }
+    //valida rg 
     if (rg.value == "" ||
         rg.value.length < 7) {
         alert("Digite os sete numeros do sue RG");
@@ -25,6 +27,7 @@ function enviar() {
 
 
     }
+    //valida se o campo cpf possui 11 digitos
     if (cpf.value == "" ||
         cpf.value.length < 11 || cpf.value.length > 11 || cpf.value == "00000000000" || cpf.value == "11111111111" || cpf.value == "22222222222" || cpf.value == "33333333333" || cpf.value == "44444444444" || cpf.value == "55555555555" || cpf.value == "66666666666" || cpf.value == "77777777777" || cpf.value == "88888888888" || cpf.value == "99999999999" || cpf.value == "12345678910") {
         alert("Por Favor digite um CPF Válido ");
@@ -32,6 +35,7 @@ function enviar() {
         return;
 
     }
+    //valida o logradouro esta preenchido
 
 
     // mensagem final se tudo preenchido ok
